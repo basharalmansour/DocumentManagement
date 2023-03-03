e77598572206fa90d8c39baa94b828c4db78d2d5
Eyad Almansour <eyadalmansour@altamira.com.tr>
2023-03-03 11:02:38 +0100

    start adding condolife dtos

---
+++ src/Application/Common/Dtos/CondoLife/ResponseDto/GetSiteDetailsResponseDto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Common.Dtos.CondoLife.ResponseDto;
+public class GetSiteDetailsResponseDto
+{
+    public Guid Id { get; set; }
+    public int? CityId { get; set; }
+    public int? DistrictId { get; set; }
+    public string Description { get; set; }
+    public string DescriptionEn { get; set; }
+    public string Email { get; set; }
+    public string Tel { get; set; }
+    public string Web { get; set; }
+    public string Address { get; set; }
+    public string AddressLat { get; set; }
+    public string AddressLong { get; set; }
+    public bool? IsCommissionSameAsParent { get; set; }
+    public bool VisitorEntryVerification { get; set; }
+    public double Commission { get; set; }
+    public List<string> Tags { get; set; }
+    public string FileURL { get; set; }
+    public Guid ParentSiteId { get; set; }
+    public string Name { get; set; }
+    public string NameEn { get; set; }
+    public string Code { get; set; }
+    public string DefaultUnitLogo { get; set; }
+    public IEnumerable<int> SegmentsMappeds { get; set; }
+    public IEnumerable<int> Languages { get; set; }
+    public int ZonesCount { get; set; }
+    public int BlockCount { get; set; }
+    public int UnitCount { get; set; }
+    public int AssetCount { get; set; }
+    public int AreaCount { get; set; }
+    public bool IsActive { get; set; }
+    public int HardwareCount { get; set; }
+    public bool IsMain { get; set; }
+    public string BusinessTitle { get; set; }
+}
--- src/Application/Common/Interfaces/ICondoLifeHttpClient.cs
+++ src/Application/Common/Interfaces/ICondoLifeHttpClient.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using CleanArchitecture.Application.Common.Dtos.Appllication;
 using CleanArchitecture.Application.Common.Dtos.CondoLife;
+using CleanArchitecture.Application.Common.Dtos.CondoLife.RequestDtos;
+using CleanArchitecture.Application.Common.Dtos.CondoLife.ResponseDto;
 
 namespace CleanArchitecture.Application.Common.Interfaces;
 
@@ -14,4 +16,20 @@ public interface ICondolifeHttpClient
     public Task<CondoUserInfoDto> GetUserInfoAsync(string userId, CancellationToken cancellationToken = default);
     public Task<CondoUserInfoDto> GetUserInfoByPhoneNumberAndEmail(string phoneNumber, string email, CancellationToken cancellationToken = default);
     public Task<bool> PublishMessage(NotificationRequestDto requestDto, CancellationToken cancellationToken = default);
+    public Task<List<GetSitesResponseDto>> GetSitesAsync(GetSitesRequestDto request,CancellationToken cancellationToken = default);
+    public Task<GetSiteDetailsResponseDto> GetSiteDetailsAsync(Guid siteId, CancellationToken cancellationToken = default);
+    public Task<bool> GetZonesAsync(CancellationToken cancellationToken = default);
+    public Task<bool> GetZoneDetailsAsync(CancellationToken cancellationToken = default);
+    public Task<bool> GetBlocksAsync(CancellationToken cancellationToken = default);
+    public Task<bool> GetBlockDetailsAsync(CancellationToken cancellationToken = default);
+    public Task<bool> GetUnitsAsync(CancellationToken cancellationToken = default);
+    public Task<bool> GetUnitDetailsAsync(CancellationToken cancellationToken = default);
+    public Task<bool> GetAreasAsync(CancellationToken cancellationToken = default);
+    public Task<bool> GetAreaDetailsAsync(CancellationToken cancellationToken = default);
+    public Task<bool> GetCompaniesAsync(CancellationToken cancellationToken = default);
+    public Task<bool> GetCompanyDetailsAsync(CancellationToken cancellationToken = default);
+    public Task<bool> GetBrandsAsync(CancellationToken cancellationToken = default);
+    public Task<bool> GetBrandDetailsAsync(CancellationToken cancellationToken = default);
+    public Task<bool> GetDepartmentsAsync(CancellationToken cancellationToken = default);
+    public Task<bool> GetPersonnelDetailsAsync(CancellationToken cancellationToken = default);
 }
