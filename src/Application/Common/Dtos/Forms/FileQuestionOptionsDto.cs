e8924dd1cb2d54d328c4570c5b14c6f88aaa29b0
Eyad <eyadmansour1993@gmail.com>
2023-03-04 13:24:40 +0300

    Question's DTOs Added

---
+++ src/Application/Common/Dtos/Forms/FileQuestionOptionsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Domain.Enums;
+
+namespace CleanArchitecture.Application.Common.Dtos.Forms;
+public class FileQuestionOptionsDto
+{
+    public DocumentFileType DocumentFileType { get; set; }
+    public int QuestionId { get; set; }
+}
