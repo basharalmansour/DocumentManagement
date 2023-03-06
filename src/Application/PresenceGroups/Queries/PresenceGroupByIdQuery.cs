e183cb33e6860d98c79a1aa04061edad89416d57
Eyad <eyadmansour1993@gmail.com>
2023-03-06 16:03:52 +0300

    Get PresenceGroup by id function added

---
+++ src/Application/PresenceGroups/Queries/PresenceGroupByIdQuery.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using CleanArchitecture.Application.Common.Dtos.PresenceGroups;
+using CleanArchitecture.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Application.PresenceGroups.Queries;
+public class PresenceGroupByIdQuery : IRequest<PresenceGroupDto>
+{
+    public int Id { get; set; }
+}
+public class PresenceGroupByIdQueryHandler : IRequestHandler<PresenceGroupByIdQuery, PresenceGroupDto>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+    private readonly IMapper _mapper;
+    public PresenceGroupByIdQueryHandler(IApplicationDbContext applicationDbContext, IMapper mapper = null)
+    {
+        _applicationDbContext = applicationDbContext;
+        _mapper = mapper;
+    }
+    public async Task<PresenceGroupDto> Handle(PresenceGroupByIdQuery request, CancellationToken cancellationToken)
+    {
+        var presenceGroup = await _applicationDbContext.PresenceGroups.FirstOrDefaultAsync(x=>x.Id==request.Id);
+        var presenceGroupDto = _mapper.Map<PresenceGroupDto>(presenceGroup);
+        return presenceGroupDto;
+    }
+}
