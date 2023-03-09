a7cab878334f53832fbf9af7c719f55e7e4e68d7
Eyad <eyadmansour1993@gmail.com>
2023-03-09 15:15:46 +0300

    VehicleController added

---
+++ src/WebUI/Controllers/Vehicles/VehicleController.cs
@@ -0,0 +1,35 @@
+﻿using CleanArchitecture.Application.Vehicles.Commands;
+using CleanArchitecture.Application.Vehicles.Queries;
+using MassTransit.Internals.GraphValidation;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.WebUI.Controllers.Vehicles;
+
+public class VehicleController : ApiControllerBase
+{
+    [HttpPost("AddVehicle")]
+    public async Task<IActionResult> CreateVehicle([FromBody] CreateVehicleCommand request , CancellationToken cancellationToken)
+    {
+        var result=  await Sender.Send(request, cancellationToken);
+        return Ok(result);
+    }
+    [HttpGet("ViewVehicles")]
+    public async Task<IActionResult> GetVehicles([FromBody] GetVehicleQuery request, CancellationToken cancellationToken)
+    {
+        var result = await Sender.Send(request, cancellationToken);
+        return Ok(result);
+    }
+    [HttpPost("EditVehicle")]
+    public async Task<IActionResult> EditVehicle([FromBody] EditVehicleCommand request, CancellationToken cancellationToken)
+    {
+        var result = await Sender.Send(request, cancellationToken);
+        return Ok(result);
+    }
+    [HttpDelete("DeleteVehicle")]
+    public async Task<IActionResult> DeleteVehicle([FromBody] RemoveVehicleCommand request, CancellationToken cancellationToken)
+    {
+        var result = await Sender.Send(request, cancellationToken);
+        return Ok(result);
+    }
+}
