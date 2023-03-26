cab8d46d7e99bb66660e17c26349c4e38ba490d4
Eyad <eyadmansour1993@gmail.com>
2023-03-26 16:45:00 +0300

    validators fixed

--- src/Application/Forms/Validators/EditFormCommandValidator.cs
+++ src/Application/Forms/Validators/EditFormCommandValidator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CleanArchitecture.Application.Common.Dtos.Forms;
 using CleanArchitecture.Application.Forms.Commands;
 using FluentValidation;
 
@@ -12,8 +13,17 @@ public class EditFormCommandValidator : AbstractValidator<EditFormCommand>
     public EditFormCommandValidator()
     {
         RuleFor(x => x.Name).MaximumLength(64).WithMessage("Name Must be between 1 and 64 character");
-        RuleFor(x => x.Questions.Select(x => x.Name)).NotNull();
+        RuleFor(x => x.Questions).Must(NamesCheck).WithMessage("All Questions must have text");
         RuleFor(x => x.Questions.Count).GreaterThan(0);
+    }
+
+    private bool NamesCheck(List<AddQuestionRequest> request)
+    {
+        foreach (var question in request)
+            if (question.Name == null)
+                return false;
+        return true;
+
     }
 }
 
