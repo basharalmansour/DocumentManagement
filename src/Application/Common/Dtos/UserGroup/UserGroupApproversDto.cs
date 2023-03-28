83c21e9ee90e8222507ec41c1bf17284123f1e3b
Eyad <eyadmansour1993@gmail.com>
2023-03-28 13:40:18 +0300

    user group approvers Query added

---
+++ src/Application/Common/Dtos/UserGroup/UserGroupApproversDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Common.Dtos.UserGroup;
+public class UserGroupApproversDto
+{
+    public int PersonnelId { get; set; }
+    public string PersonnelName { get; set; }
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+}
